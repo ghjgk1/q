fb60bf994fe6bfc2ab2474e0d484a0ebe95fbd48
ghjgk1 <154216526+ghjgk1@users.noreply.github.com>
2024-09-04 10:13:19 +0500

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NikitinSA_PR-31_01"
+include(":app")
+ 
